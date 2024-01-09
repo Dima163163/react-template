57d1d6f3418bed3fdad33eea46136fadda304408
Dima163163 <khlopovdima@gmail.com>
2024-01-09 13:53:12 +0400

    react-template

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import elka from './img/photo.png'
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img width={500} src={elka} />
+  </>
+)
+
+export default Module;
